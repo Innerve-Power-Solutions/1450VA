29e798e3b244f35a30f7725ad986a013f0519bed
saikrishna.gundlapalli <kashyap@innerve.co.in>
2016-09-30 15:55:17 +0530

    INVMAX, 1.01
    Updates:
    * Included git version control and product IDs for 1450VA and 1800VA
    * Version release is based on software used for demo on 15th July 2016
    
    Remaining Remarks:
    * DC link sense correction
    * DC link limits modification for Inverter ON and OFF control
    * ADC sampling improvisation
    * Buzzer and Fan pin reallocation according to new boards
    * LCD display changes
    * Filter for grid sensing signal
    * Inverter pulses ON and OFF control improvisation
    * Algorithm for fans turn ON and turn OFF
    * DC link current sense correction
    * Feature for PF estimation
    * Over temperature algorithm clean up
    * Over load and under voltage battery algorithms validation

---
+++ INVMAX1450VA_150716.X/config.h
@@ -0,0 +1,49 @@
+/*****************************************************************************/
+/*                                                                           */
+/*                      INNERVE POWER SOLUTIONS                          */
+/*                                                                           */
+/*****************************************************************************/
+/*                                                                           */
+/* Module name:         CONFIG.H                                             */
+/*                                                                           */
+/* Programmer:          Saikrishna Kashyap G                                 */
+/*                                                                           */
+/* Date:                2016-09-30                                           */
+/*                                                                           */
+/* Description:         Defines the product ID and release type of firmware  */
+/*                                                                           */
+/*****************************************************************************/
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef CONFIG_HEADER_TEMPLATE_H
+#define	CONFIG_HEADER_TEMPLATE_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+#include <stdint.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#define VER_RELEASE             0
+#define VER_PRE_RELEASE         1
+#define VER_BETA                2
+#define VER_ALFA                3
+
+#define RELEASE_TYPE            VER_RELEASE
+
+#define VERSION_MAJOR           1
+#define VERSION_MINOR           1
+
+// 1450VA or 1800VA
+#define PRODUCT_ID_1450VA       1
+#define PRODUCT_ID_1800VA       2
+
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
